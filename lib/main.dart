771aab38aa60596b500393c19ab3623a6ebf6c07
ttoghere <tuncsoftwarehere@gmail.com>
2022-08-12 02:43:12 +0300

    users firebase done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:ecomflutter3users/firebase_options.dart';
 import 'package:ecomflutter3users/main_screens/auth/auth_screen.dart';
 import 'package:ecomflutter3users/main_screens/ms_shelf.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
@@ -12,7 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Ecom - Users',
-     initialRoute: AuthScreen.routeName,
+      initialRoute: AuthScreen.routeName,
       routes: {
         HomeScreen.routeName: (context) => const HomeScreen(),
         AuthScreen.routeName: (context) => const AuthScreen(),
